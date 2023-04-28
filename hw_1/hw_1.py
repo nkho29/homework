51bcfd29ace5a9adbfddba62e7f8b1fb67a4f50a
nkho29 <nazar17ho@gmail.com>
2023-04-28 09:37:22 +0300

    hw_1

--- hw_1/hw_1.py
+++ hw_1/hw_1.py
@@ -1,5 +1,9 @@
 print("homework 1")
 print("working")
 print("letters".upper())
-
-
+homework_1 = "complete"
+print(type(homework_1))
+print(id(homework_1))
+# and elif if raise assert except
+letters = "abcdefg"
+print(letters.upper())
