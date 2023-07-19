51414a6c5cb326b6cf2aa04aa957f351614455c1
nkho29 <nazar17ho@gmail.com>
2023-07-19 15:35:05 +0300

    hw_8

---
+++ hw_11.py
@@ -0,0 +1,16 @@
+for i in 1,2,3,'one','two','three':
+    print(i)
+
+
+for i in range(4):
+    print(i)
+    print(i**2)
+print("End...")
+
+
+sum = 0
+n = 5
+for i in range(1, n+1):
+    sum +=i
+print(sum)
+
