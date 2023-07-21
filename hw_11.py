5af9198f6808988cbddd5cad47e31fb9c6a180c3
nkho29 <nazar17ho@gmail.com>
2023-07-21 14:45:05 +0300

    hw_11B

--- hw_11.py
+++ hw_11.py
@@ -14,3 +14,28 @@ for i in range(1, n+1):
     sum +=i
 print(sum)
 
+
+numbers = [12, 25, 98, 156, 36, 87]
+sum_of_numbers = 0
+for number in numbers:
+    sum_of_numbers += number
+print(sum_of_numbers)
+
+
+names = ["Lewis", "Max", "Logan", "Alex", "George"]
+for name in enumerate(names,1):
+    print(name)
+
+
+x = int(input("Number:"))
+squares = []
+
+for num in range(1,x):
+    if num > 0:
+        squares.append(num ** 2)
+
+print("Квадрати додатних чисел менших за", x, ":", squares)
+
+
+for num in range(15, 0, -1):
+    print(num, end=" ")
