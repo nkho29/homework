554338ffdb0d92e76547775f96083f1ef1af1a08
nkho29 <nazar17ho@gmail.com>
2023-08-08 09:44:53 +0300

    hw14_chngd

--- hw_14.py
+++ hw_14.py
@@ -3,14 +3,15 @@ import time
 from datetime import date, timedelta
 
 # current_date = time.strftime("%Y %d %B %A, %H; %M; %S")
-now = date.today()
+now = datetime.datetime.now()
 
 current_date = time.strftime("%Y %d %B %A")
 
 current_time = time.strftime("%H:%M")
 
-one_day = timedelta(days=1, hours=12, minutes=30)
-two_days = timedelta(days=2, hours=17, minutes=00)
+one_day = timedelta(days=0, hours=8, minutes=0)
+two_days = timedelta(days=1, hours=10, minutes=30)
+arrive_time = timedelta(hours=15)
 
 tomorrow = now + one_day
 
@@ -21,3 +22,5 @@ arrive_day = now + two_days
 arrive_time = arrive_day.strftime("%H:%M")
 
 print(f"Сьогодні {current_date, current_time} . Завтра о {tomorrow_time} я їду за місто. Повертаюсь через день {arrive_day, arrive_time}")
+
+print(now.today())
