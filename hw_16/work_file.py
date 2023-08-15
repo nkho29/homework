6f6b7136aed6a07d7ba58debc9fdd274ca89d8be
nkho29 <nazar17ho@gmail.com>
2023-08-15 11:52:39 +0300

    hw16

---
+++ hw_16/work_file.py
@@ -0,0 +1,46 @@
+import json
+import csv
+import datetime
+import time
+
+#
+# person_info = {
+#     "Age": "15",
+#     "Name": "Nazar",
+#     "Surname": "Khomych"
+# }
+#
+# print(type(person_info))
+#
+# prsninf_data = json.dumps(person_info)
+#
+# print(type(prsninf_data))
+#
+# with open("empty_json.json", "w+") as file:
+#     json.dump(person_info, file, indent=4)
+
+
+
+current_time = datetime.datetime.now()
+# current_time = time.localtime()
+str_time = str(current_time)
+print(current_time)
+print(str_time)
+
+with open("empty_txt.txt", "w") as file:
+     file.write(str_time)
+     file.seek(0)
+
+
+shop_info = [
+    ["OBJECT", "PRICE", "QUANTITY"],
+    ["Apples", 10, "kg"],
+    ["Oranges", 12, "2kg"],
+    ["Bananas", 8, "kg"],
+    ["Strawberries", 14, "2kg"]
+]
+
+with open("empty_csv.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(shop_info[0])
+    writer.writerows(shop_info[1:])
